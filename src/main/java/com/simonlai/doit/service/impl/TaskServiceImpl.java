4f607f8a0a280eb849dc3cf02c153c11a77bd7cf
Simon Lai <f88083@gmail.com>
2024-07-25 20:41:51 +0800

    feat: implemented getAllTasks in TaskServiceImpl.java

--- src/main/java/com/simonlai/doit/service/impl/TaskServiceImpl.java
+++ src/main/java/com/simonlai/doit/service/impl/TaskServiceImpl.java
@@ -1,13 +1,21 @@
 package com.simonlai.doit.service.impl;
 
+import com.simonlai.doit.repository.TaskRepository;
 import com.simonlai.doit.model.Task;
 import com.simonlai.doit.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
     @Override
     public List<Task> getAllTasks() {
-        return List.of();
+        return taskRepository.findAll();
     }
 }
