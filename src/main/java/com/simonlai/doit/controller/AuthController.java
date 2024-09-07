088da183801b8383a7a866e999a258e717b0ab95
Simon Lai <f88083@gmail.com>
2024-09-07 14:37:16 +0800

    feat: genuine check the username and password

--- src/main/java/com/simonlai/doit/controller/AuthController.java
+++ src/main/java/com/simonlai/doit/controller/AuthController.java
@@ -4,13 +4,16 @@ import com.simonlai.doit.dto.LoginRequest;
 import com.simonlai.doit.dto.RegisterRequest;
 import com.simonlai.doit.model.User;
 import com.simonlai.doit.repository.UserRepository;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
@@ -36,7 +39,20 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
-        // Authentication is handled by Spring Security
-        return ResponseEntity.ok("User logged in successfully!");
+        // Check user existent by username
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElse(null);
+
+        // Authenticate password
+        if (user != null && passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            // Successfully authenticated
+            return ResponseEntity.ok().body(Map.of(
+                    "message", "User logged in successfully!",
+                    "username", user.getUsername()));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of(
+                            "message", "Invalid username or password"));
+        }
     }
 }
