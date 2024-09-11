e5090c039638444783482c08c921e006d4198e15
Simon Lai <f88083@gmail.com>
2024-09-11 16:26:47 +0800

    feat: disable session creation

--- src/main/java/com/simonlai/doit/security/config/SecurityConfig.java
+++ src/main/java/com/simonlai/doit/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.simonlai.doit.security.jwt.JwtRequestFilter;
@@ -25,7 +26,8 @@ public class SecurityConfig {
                 .anyRequest().authenticated())
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // Add jwt filter before UsernamePassword
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
